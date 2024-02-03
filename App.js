7ecaf96b34a2c389e4ec45becb92bbe17aecdd76
punnyapradeepe <118207329+punnyapradeepe@users.noreply.github.com>
2024-02-03 12:20:16 +0530

    Update App.js
    Corrected the error : Android Bundling failed 8649ms (C:\Users\ACER\OneDrive\Desktop\woxro_my_app\woxro_design\node_modules\expo\AppEntry.js)
    Unable to resolve "./App/Navigativigation" from "App.js"

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Homescreen from './App/Screens/HomeScreen/HomeScreen';
-import TabNavigation from './App/Navigativigation';
 
 export default function App() {
   return (
